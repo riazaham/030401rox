a2405760cfbae7262358e79303e7b7c10fb9ed56
kaiwen98 <e0406826@u.nus.edu>
2020-03-16 11:41:17 +0800

    Version 4.0

--- 03-04-01/w8s1pi/Alex/Alex/Alex.ino
+++ 03-04-01/w8s1pi/Alex/Alex/Alex.ino
@@ -20,13 +20,13 @@ volatile TDirection dir = STOP;
 // Number of ticks per revolution from the 
 // wheel encoder.
 
-#define COUNTS_PER_REV      1
+#define COUNTS_PER_REV      180
 
 // Wheel circumference in cm.
 // We will use this to calculate forward/backward distance traveled 
 // by taking revs * WHEEL_CIRC
 
-#define WHEEL_CIRC          1
+#define WHEEL_CIRC          22
 
 // Motor control pins. You need to adjust these till
 // Alex moves in the correct direction
@@ -183,7 +183,8 @@ void enablePullups()
   // Use bare-metal to enable the pull-up resistors on pins
   // 2 and 3. These are pins PD2 and PD3 respectively.
   // We set bits 2 and 3 in DDRD to 0 to make them inputs. 
-  
+  DDRD &= ~((1<<2)|(1<<3));
+  PORTD |= (1<<2)|(1<<3);
 }
 
 // Functions to be called by INT0 and INT1 ISRs.
@@ -205,8 +206,8 @@ void leftISR()
   }
     
   //leftTicks++;
-  Serial.print("LEFT: ");
-  Serial.println(leftTicks);
+  /*Serial.print("LEFT: ");
+  Serial.println(leftTicks);*/
 }
 
 void rightISR()
@@ -221,8 +222,8 @@ void rightISR()
 
   
   //rightTicks++;
-  Serial.print("RIGHT: ");
-  Serial.println(rightTicks);
+  /*Serial.print("RIGHT: ");
+  Serial.println(rightTicks);*/
 }
 
 // Set up the external interrupt pins INT0 and INT1
@@ -603,13 +604,13 @@ void loop() {
 
 // Uncomment the code below for Step 2 of Activity 3 in Week 8 Studio 2
 
-// forward(0, 100);
+//forward(0, 100);
 
 // Uncomment the code below for Week 9 Studio 2
 
 
  // put your main code here, to run repeatedly:
-  TPacket recvPacket; // This holds commands from the Pi
+ TPacket recvPacket; // This holds commands from the Pi
 
   TResult result = readPacket(&recvPacket);
   
